b8856d39d8ecbb720d32e5bcf03cb6890906d50d
AriaFallah <aria@fallah.us>
2016-05-09 18:28:50 -0400

    :art: make type Int

--- SingleNumber3.hs
+++ SingleNumber3.hs
@@ -2,6 +2,6 @@ module LC where
 
 import Data.Map (fromListWith, toList)
 
-singleNumber :: [Integer] -> [Integer]
+singleNumber :: [Int] -> [Int]
 singleNumber xs = fst <$> filter (\(_, y) -> y == 1) frequency
   where frequency = toList (fromListWith (+) [(x, 1) | x <- xs])
