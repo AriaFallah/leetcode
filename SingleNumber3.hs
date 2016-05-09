b0b91d67de3239c79f07af8b1c013c553558e25b
AriaFallah <aria@fallah.us>
2016-05-09 00:24:27 -0400

    :art: use lambda for filtering function

--- SingleNumber3.hs
+++ SingleNumber3.hs
@@ -2,8 +2,6 @@ module LC where
 
 import Data.Map (fromListWith, toList)
 
-singleNumber :: [Int] -> [Int]
-singleNumber xs = fst <$> filter f frequency
-  where
-    frequency = toList (fromListWith (+) [(x, 1) | x <- xs])
-    f (_, y) = y == 1
+singleNumber :: [Integer] -> [Integer]
+singleNumber xs = fst <$> filter (\(_, y) -> y == 1) frequency
+  where frequency = toList (fromListWith (+) [(x, 1) | x <- xs])
