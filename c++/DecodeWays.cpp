b62363cba5e9df0746f62bd8b41069f32a18594f
AriaFallah <aria@fallah.us>
2016-09-25 14:22:13 -0400

    :new: MoveZeroes and try to grok DP in DecodeWays

---
+++ c++/DecodeWays.cpp
@@ -0,0 +1,49 @@
+// https://leetcode.com/problems/decode-ways/
+    
+/*
+  A message containing letters from A-Z is being encoded to numbers using the following mapping:
+
+  'A' -> 1
+  'B' -> 2
+  ...
+  'Z' -> 26
+
+  Given an encoded message containing digits, determine the total number of ways to decode it.
+
+  For example,
+  Given encoded message "12", it could be decoded as "AB" (1 2) or "L" (12).
+
+  The number of ways decoding "12" is 2.
+*/
+
+#include <string>
+#include <iostream>
+
+class Solution {
+public:
+  int numDecodings(std::string s) {
+    if (s.empty() || s[0] == '0') return 0;
+
+    int ways[3] = { 0, 1, 1 };
+    int letters[2] = { 0, s[0] - '0' };
+    
+    for (size_t i = 1, len = s.length(); i < len; ++i) {
+      ways[0] = ways[1];
+      ways[1] = ways[2];
+      letters[0] = letters[1];
+      letters[1] = s[i] - '0';
+      
+      ways[2] =
+        ways[1] * (letters[1] != 0)
+        + ways[0] * (letters[0] != 0 && letters[0] * 10 + letters[1] <= 26);
+    }
+
+    return ways[2];
+  }    
+};
+
+int main() {
+  Solution s;
+  int result = s.numDecodings("1234");
+  std::cout << result << std::endl;
+}
--- c++/MoveZeros.cpp
+++ c++/MoveZeros.cpp
@@ -2,16 +2,41 @@
 
 #include <vector>
 #include <utility>
+#include <iostream>
+
+/*
+  Given an array nums, write a function to move all 0's to the end of it while maintaining
+  the relative order of the non-zero elements.
+
+  For example, given nums = [0, 1, 0, 3, 12], after calling your function, nums should 
+  be [1, 3, 12, 0, 0].
+
+  Note:
+  You must do this in-place without making a copy of the array.
+  Minimize the total number of operations.
+*/
 
 class Solution {
 public:
   void moveZeroes(std::vector<int>& nums) {
-    for (int i = 1; i < nums.size(); ++i) {
-      for (int j = i; j > 0; --j) {
-        if (nums[j - 1] == 0) {
-          std::swap(nums[j], nums[j - 1]);
-        } else break;
-      }
+    size_t size = nums.size();
+    if (size < 2) return;
+    
+    unsigned p1 = 0;
+    unsigned p2 = 1;
+    
+    for (;;) {
+      while (nums[p1] != 0) p1++;
+      while (nums[p2] == 0 || p2 < p1) p2++;
+      if (p2 < size) std::swap(nums[p1++], nums[p2++]);
+      else break;
     }
   }
 };
+
+int main() {
+  Solution s;
+  std::vector<int> v = { 0, 1, 0, 3, 12 };
+  s.moveZeroes(v);
+  for (auto x : v) std::cout << x << " ";
+}
