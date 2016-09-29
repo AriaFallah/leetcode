a42b6869f1c55ecd0054ad64acf45ba56fe38ab4
AriaFallah <aria@fallah.us>
2016-09-28 22:38:06 -0400

    :new: ugly way to do letter combinations

---
+++ c++/LetterCombinationsOfAPhoneNumber.cpp
@@ -0,0 +1,67 @@
+// https://leetcode.com/problems/letter-combinations-of-a-phone-number/
+
+/*
+  Given a digit string, return all possible letter combinations that the number could represent.
+  A mapping of digit to letters (just like on the telephone buttons) is given below.
+
+  Input:Digit string "23"
+  Output: ["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"].
+*/
+
+#include <vector>
+#include <string>
+#include <iostream>
+
+using std::vector;
+using std::string;
+using std::cout;
+using std::endl;
+
+
+class Solution {
+public:
+  vector<string> letterCombinations(string digits) {
+    if (digits.length() == 0) return {};
+    
+    vector<string> res;
+    vector<vector<string>> sets;
+    vector<string> mappings[8] = {
+      { "a", "b", "c" },
+      { "d", "e", "f" },
+      { "g", "h", "i" },
+      { "j", "k", "l" },
+      { "m", "n", "o" },
+      { "p", "q", "r", "s"},
+      { "t", "u", "v" },
+      { "w", "x", "y", "z" }
+    };
+
+    for (char c : digits) {
+      if (c >= '2' && c <= '9') {
+       sets.push_back(mappings[c - '0' - 2]); 
+      }
+    }
+
+    res = sets[0];
+    for (size_t i = 1, len = sets.size(); i < len; ++i) {
+      res = generateCombos(res, sets[i]);
+    }
+
+    return res;
+  }
+private:
+  vector<string> generateCombos(vector<string> const& prev, vector<string> const& cur) {
+    vector<string> res;
+    for (string s : prev) {
+      for (string s2 : cur) {
+        res.push_back(s + s2);
+      }
+    }
+    return res;
+  }
+};
+
+int main() {
+  Solution s;
+  for (auto x : s.letterCombinations("")) cout << x << endl;
+}
