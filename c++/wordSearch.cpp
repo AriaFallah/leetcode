8a7a6a50cb4dabe758dbb3680d9c62c538ed6fb6
AriaFallah <aria@fallah.us>
2016-10-02 23:59:09 -0400

    :art: version of mutable word search without TLE

--- c++/wordSearch.cpp
+++ c++/wordSearch.cpp
@@ -21,62 +21,45 @@
 
 #include <string>
 #include <vector>
-#include <unordered_set>
 
 using std::vector;
 using std::string;
-using std::unordered_set;
-
-struct Loc {
-  // Attribues
-  int m_row;
-  int m_col;
-
-  // Constuctor
-  Loc(int row, int col): m_row(row), m_col(col) {}
-
-  // Equality operator
-  bool operator==(Loc const& l) const {
-    return m_row == l.m_row && m_col == l.m_col;
-  }
-};
-
-struct loc_hash {
-  size_t operator()(Loc const& l) const {
-    return l.m_row * 31 + l.m_col;
-  }
-};
 
 class Solution {
 public:
   bool exist(vector<vector<char>>& board, string word) {
     if (board.size() == 0) return false;
     
-    unordered_set<Loc, loc_hash> visited;
     for (int row = 0; row < (int)board.size(); ++row) {
       for (int col = 0; col < (int)board[0].size(); ++col) {
-        if (search(board, word, visited, Loc(row, col), 0)) return true;
+        if (search(board, word, row, col, 0)) return true;
       }
     }
 
     return false;
   }
 private:
-  bool search (vector<vector<char>> const& b, string const& word, unordered_set<Loc, loc_hash> v, Loc l, int pos) {
+  bool search (vector<vector<char>>& b, string const& word, int row, int col, int pos) {
     if (pos == (int)word.length()) return true;
     if (
-      v.find(l) != v.end()
-      || l.m_row < 0
-      || l.m_col < 0
-      || l.m_row == (int)b.size()
-      || l.m_col == (int)b[0].size()
-      || b[l.m_row][l.m_col] != word[pos]
+      row < 0
+      || col < 0
+      || row == (int)b.size()
+      || col == (int)b[0].size()
+      || b[row][col] != word[pos]
     ) return false;
 
-    v.insert(l);
-    return search(b, word, v, Loc(l.m_row + 1, l.m_col), pos + 1)
-        || search(b, word, v, Loc(l.m_row - 1, l.m_col), pos + 1)
-        || search(b, word, v, Loc(l.m_row, l.m_col + 1), pos + 1)
-        || search(b, word, v, Loc(l.m_row, l.m_col - 1), pos + 1);
+    // Save the value of the board and "visit" it
+    char val = b[row][col];
+    b[row][col] = '\0';
+    
+    bool res = search(b, word, row + 1, col, pos + 1)
+        || search(b, word, row - 1, col, pos + 1)
+        || search(b, word, row, col + 1, pos + 1)
+        || search(b, word, row, col - 1, pos + 1);
+
+    // Restore value to old value and return
+    b[row][col] = val;
+    return res;
   }
 };
