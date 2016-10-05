b48a31d46f26796c887d4774a5e75ec6dbbc0301
AriaFallah <aria@fallah.us>
2016-10-04 23:15:32 -0400

    :art: review LCA and make code harder to read

--- c++/BTLCA.cpp
+++ c++/BTLCA.cpp
@@ -1,14 +1,20 @@
 // https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-tree/
 
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
- * };
- */
+/*
+  Given a binary tree, find the lowest common ancestor (LCA) of two given nodes in the tree.
+
+  According to the definition of LCA on Wikipedia: “The lowest common ancestor is defined 
+  between two nodes v and w as the lowest node in T that has both v and w as descendants 
+  (where we allow a node to be a descendant of itself).”
+*/
+
+struct TreeNode {
+  int val;
+  TreeNode* left;
+  TreeNode* right;
+  explicit TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+};
+
 class Solution {
 public:
   TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
@@ -17,7 +23,6 @@ public:
     TreeNode* left = lowestCommonAncestor(root->left, p, q);
     TreeNode* right = lowestCommonAncestor(root->right, p, q);
 
-    if (left && right) return root;
-    return left ? left : right;
+    return left && right ? root : left ? left : right;
   }
 };
